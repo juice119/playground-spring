0d14ee0398f376656fc952dcd58055af14c28e70
Juice <3221ju@gmail.com>
2024-01-25 14:01:55 +0900

    feat: DB 기초 세팅

---
+++ jdbc/sql/init.sql
@@ -0,0 +1,18 @@
+CREATE TABLE users(
+	id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+	phone varchar(11) NOT NULL,
+	name varchar(10) NOT NULL,
+	last_account_record_id BIGINT NOT NULL,
+	PRIMARY KEY(id),
+	UNIQUE KEY(phone)
+);
+
+
+CREATE TABLE account_records(
+	id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+	balance int NOT NULL,
+	change_amount int NOT NULL,
+	source_user_id BIGINT,
+	operation_type VARCHAR(20),
+	PRIMARY KEY(id)
+);
